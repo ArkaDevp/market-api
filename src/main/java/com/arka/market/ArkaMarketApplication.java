4060c1c5b43043aa8c357a74a8939387659c30ba
ArkaDev <29paisa@gmail.com>
2026-02-18 17:51:08 -0500

    First commit: Spring project configuration

---
+++ src/main/java/com/arka/market/ArkaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.arka.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arka/market/ArkaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arka.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
